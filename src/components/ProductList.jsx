d55c4f78308ca4e5ded081b4ef4d853104b1fe9b
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-12-23 14:08:54 +0100

    finalizzazione milestone 2 (Aggiungere prodotti al carrello)

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,23 +1,53 @@
-function ProductList() {
-    
-    const products = [
-  { name: 'Mela', price: 0.5 },
-  { name: 'Pane', price: 1.2 },
-  { name: 'Latte', price: 1.0 },
-  { name: 'Pasta', price: 0.7 },
-];
+import { useState } from "react";
 
-  return (
-    <>
-    <h1>Lista Prodotti</h1>
-    <ul>
-      {products.map((prod, index) => (
-        <li key={index}><strong>{prod.name}</strong> Prezzo: {prod.price}€</li>
-      )
-    )}
-    </ul>
-    </>
-  )
+function ProductList() {
+
+    const products = [
+        { name: 'Mela', price: 0.5 },
+        { name: 'Pane', price: 1.2 },
+        { name: 'Latte', price: 1.0 },
+        { name: 'Pasta', price: 0.7 },
+    ];
+
+    const [addedProducts, setAddedProducts] = useState([]);
+
+    const addToCart = (prod) => {
+        const exists = addedProducts.some(item => item.name === prod.name);
+
+        if (exists) {
+            alert(`${prod.name} è già nel carrello!`);
+            return;
+        }
+        setAddedProducts(prev => [...prev, { ...prod, quantity: 1 }]);
+    };
+
+    return (
+        <>
+            <h1>Lista Prodotti</h1>
+            <ul>
+                {products.map((prod, index) => (
+                    <li key={index}>
+                        <strong>{prod.name}</strong>
+                        Prezzo: {prod.price}€
+                        <button id="bottone" onClick={() => addToCart(prod)}>Aggiungi al Carrello</button>
+                    </li>
+                )
+                )}
+            </ul>
+            <h2>Carrello</h2>
+            <ul>
+                {addedProducts.map((prod, index) => (
+                    <li key={index}>
+                        <strong>{prod.name} </strong>
+                        Prezzo: {prod.price}€
+                        Quantità: {prod.quantity}
+                    </li>
+                )
+                )}
+            </ul>
+
+        </>
+    )
 }
 
 export default ProductList
